cde4ae01fad0efae45fc67db2b2060905847467e
BryantChi <bryantchi.work@gmail.com>
2024-05-25 15:34:02 +0800

    [MOD] modify admin 公司資訊 to 聯絡資訊

--- resources/views/admin/company_infos/create.blade.php
+++ resources/views/admin/company_infos/create.blade.php
@@ -5,7 +5,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-12">
-                    <h1>公司資訊 - 新增</h1>
+                    <h1>聯絡資訊 - 新增</h1>
                 </div>
             </div>
         </div>
--- resources/views/admin/company_infos/show.blade.php
+++ resources/views/admin/company_infos/show.blade.php
@@ -5,7 +5,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1>公司資訊 - 顯示</h1>
+                    <h1>聯絡資訊 - 顯示</h1>
                 </div>
                 <div class="col-sm-6">
                     <a class="btn btn-default float-right"
