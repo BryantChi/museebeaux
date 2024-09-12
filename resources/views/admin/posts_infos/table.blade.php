5e98da696fe5a71828ec3941957c94119a10ac03
BryantChi <bryantchi.work@gmail.com>
2024-09-12 23:49:41 +0800

    [MOD] 新增文章分類v2

--- resources/views/admin/posts_infos/table.blade.php
+++ resources/views/admin/posts_infos/table.blade.php
@@ -3,7 +3,7 @@
         <a href="{{ route('admin.postsInfos.index') }}" class="btn btn-outline-secondary mb-3">重新整理</a>
         <div class="form-group row justify-content-end align-content-center">
             {!! Form::label('post_type', '文章分類篩選 ', ['class' => 'col-auto mb-0 col-form-label']) !!}
-            <div class="col-auto">
+            <div class="col-auto" style="min-width: 160px;">
                 {!! Form::select('post_type', ['' => '請選擇'] + (DB::table('post_type_infos')->whereNull('deleted_at')->where('type_parent_id', null)->orderBy('type_name')->pluck('type_name', 'id')->toArray() ?? []), request()->get('post_type') ?? null, [
                     'class' => 'form-control',
                     'onchange' => 'fetchPostsInfos(this.value)'
