7cb60a51d031ddc1e1b1a4b0cc51411ba9e93426
BryantChi <bryantchi.work@gmail.com>
2025-02-12 11:31:18 +0800

    [MOD] 調整301轉址功能

--- resources/views/admin/redirects_infos/fields.blade.php
+++ resources/views/admin/redirects_infos/fields.blade.php
@@ -1,11 +1,17 @@
 <!-- Old Url Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('old_url', 'Old Url:') !!}
-    {!! Form::text('old_url', null, ['class' => 'form-control']) !!}
+    {!! Form::text('old_url', null, ['class' => 'form-control', 'placeholder' => '請輸入原始網址，例如：/xxx/xxx/xxx']) !!}
+    <small class="form-text text-muted">
+        若您的轉址僅針對本站點，建議只輸入以「/」開頭的路徑。
+    </small>
 </div>
 
 <!-- New Url Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('new_url', 'New Url:') !!}
-    {!! Form::text('new_url', null, ['class' => 'form-control']) !!}
+    {!! Form::text('new_url', null, ['class' => 'form-control', 'placeholder' => '請輸入原始網址，例如：/xxx/xxx/xxx']) !!}
+    <small class="form-text text-muted">
+        若您的轉址僅針對本站點，建議只輸入以「/」開頭的路徑。
+    </small>
 </div>
