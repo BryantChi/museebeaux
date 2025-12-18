f71c128c1f1c59af1cdae24e24c173f555b6af6c
BryantChi <bryantchi.work@gmail.com>
2025-12-18 09:47:50 +0800

    [MOD] 修正文章預覽文字上限

--- resources/views/services_grid.blade.php
+++ resources/views/services_grid.blade.php
@@ -44,7 +44,7 @@
                                         <h2>{{ $post->post_title }}</h2>
                                     </a>
                                     <p class="multiline-ellipsis">
-                                        {!! str_replace(["\r\n", "\r", "\n"], '', strip_tags($post->post_content)) !!}</p>
+                                        {!! Str::limit(str_replace(["\r\n", "\r", "\n"], '', strip_tags($post->post_content)), 100) !!}</p>
                                     {{-- <ul class="blog-info-link mt-3 mb-4 ml-auto list-unstyled">
                                         <li><a href="{{ route('services.items', DB::table('post_type_infos')->whereNull('deleted_at')->where('id', $post->post_type)->value('type_slug'))  }}"><i class="fas fa-flag"></i>
                                                 {{ DB::table('post_type_infos')->whereNull('deleted_at')->where('id', $post->post_type)->value('type_name') }}</a>
